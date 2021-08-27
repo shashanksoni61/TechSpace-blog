7878e4bb34eb7c08b92c05b3cb950aeed9be3950
shashanksoni61 <shashanksoni61@gmail.com>
2021-08-28 02:04:54 +0530

    Next JS boiler-plate

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>This is HomePage</h1>
+    </div>
+  );
+}
